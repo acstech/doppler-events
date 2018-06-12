c3d0161a5c24b0e52cb0dfe60a7af15631d9b00f
Matt Smith <38513513+mattsmith803@users.noreply.github.com>
2018-06-12 13:35:30 -0400

    added more documentation (#27)

--- eventAPI_server/EventAPIServer.go
+++ eventAPI_server/EventAPIServer.go
@@ -1,7 +1,6 @@
 // Event API Server
-//
-//
-//
+// Files Needed: EventAPIClient.go, EventAPIServer.go, eventAPI.pb.go
+// About: this program receives a message from EventAPIClient.go, formats the data to JSON, and sends the data to Kafka Connect
 //
 
 package main
@@ -17,55 +16,65 @@ import (
 	"github.com/golang/protobuf/ptypes"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
+//intialize addressess of where server listens with format IP:Port
 const (
-	// addr = "10.22.97.107:8080"
-	addr = ":8080"
+	// address = "10.22.97.107:8080"
+	address = ":8080"
 )
 
+//struct to hold parameters for server
 type server struct{}
 
+// SendEvent is the function that EventAPIClient.go calls in order to send data to the server
+// the data is then processed, formatted to JSON, and send to Kafka Connect
 func (s *server) SendEvent(ctx context.Context, in *pb.EventObj) (*pb.EventResp, error) {
-	//printing ClientID and EventID to server console
+	//get the current time
 	ts, err := ptypes.Timestamp(in.TimeSinceEpoch)
 	if err != nil {
 		return nil, err
 	}
+
+	//printing ClientID and EventID to server console for testing
 	fmt.Printf("ClientID: " + in.ClientID + "\nEventID: " + in.EventID + "\nDate: " + ts.String() + "\n")
 
-	//printing DataSet to server console
+	//printing DataSet to server console for testing
 	for key, value := range in.DataSet {
 		fmt.Println("DataType: ", key, "DataValue:", value)
 	}
 	fmt.Println()
 
-	//method to format to JSON
+	//format to JSON
 	bytes, err := json.Marshal(in) //Marshal returns the ascii presentation of the data
 	if err != nil {
 		fmt.Println("Format to JSON Error")
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	//print JSON format of
+	//print JSON format of dataSet to console for testing
 	fmt.Println(string(bytes))
 
-	//method to send to Kafka
+	//NEEDED method to send to Kafka
 
 	//return response to client
 	return &pb.EventResp{Response: "Success! Open heatmap at ____ to see results"}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", addr)
+	//initialize listener on server address
+	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	//initialize server
 	s := grpc.NewServer()
+	//register server to grpc
 	pb.RegisterEventSenderServer(s, &server{})
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
+	// Register reflection service on gRPC server for back and forth communication. Kept for future use if necessary
+	// reflection.Register(s)
+
+	//tells the server to process the incoming messages, checks if failed to serve
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
