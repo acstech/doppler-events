96e2d3987803d4a78e0126c4e3f40438a82352d5
MattS <matts@acst.com>
2018-06-12 15:42:18 -0400

    flattened EventObj so could marshal to a flat JSON for InfluxDB, added sample data options to testUser.go

--- testUser.go
+++ testUser.go
@@ -21,11 +21,14 @@ func main() {
 	dataValue1 := "665.67676"
 	dataName2 := "lat"
 	dataValue2 := "87687"
+	// dataName3 := "gender"
+	// dataValue3 := "male"
 
 	//create map of data
 	dataSet := make(map[string]string)
 	dataSet[dataName1] = dataValue1
 	dataSet[dataName2] = dataValue2
+	// dataSet[dataName3] = dataValue3
 
 	//call to API, requires clientID, eventID, and a dataSet
 	c.DisplayData(&clientID, &eventID, &dataSet)
