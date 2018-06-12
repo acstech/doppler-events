0f645686b47a62a29765f4776b89478a56d0f208
MattS <matts@acst.com>
2018-06-12 13:33:23 -0400

    added more documentation

--- eventAPI_client/EventAPIClient.go
+++ eventAPI_client/EventAPIClient.go
@@ -1,9 +1,21 @@
 // Event API Client
+// Files Needed: EventAPIClient.go, EventAPIServer.go, eventAPI.pb.go
+// About: this program takes in a clientID *string, eventID *string, and a *map[dataName string]dataValue string
+// and enables the data to be displayed on the heatmap found at _____ ********
 //
+// Specifically this program sends data to Kafka so that it can be processed before displaying
 //
+// How to test:
+// start server with "go run eventAPI_server"
+// start client with "go run testUser.go"
 //
+// How to use:
+// Import this package via "github.com/acstech/doppler-events/eventAPI_client"
+// Initialize clientID and eventID
+// Initialize a map with the dataName as the key and the dataValue as the value
+// Call c.DisplayData(clientID *string, eventID *string, dataSet *map[dataName string]dataValue string with the required arguments
 //
-//
+
 package client
 
 import (
@@ -16,13 +28,16 @@ import (
 	"google.golang.org/grpc"
 )
 
+//intialize address of Kafka connect
 const (
 	address = "localhost:8080"
-	// address     = "address to server"
+	// address = "kafka IP:Port"
 )
 
+//DisplayData takes in the required arguments and sends the arguments to the server
 func DisplayData(clientID *string, eventID *string, dataSet *map[string]string) {
 
+	//intialize connection with server at address
 	conn, err := grpc.Dial(address, grpc.WithInsecure()) //WithInsecure meaning no authentication required
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
