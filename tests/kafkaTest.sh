e8700a83683b61636d30e5af2953770bc19a0354
pjkaufman <PeterJamesKaufman@gmail.com>
2018-06-14 11:32:56 -0400

    Updated docker
    Added couchbase to docker and a script file for influxdb's startup

---
+++ tests/kafkaTest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Kafka test begins..."
+docker-compose exec kafka  \
+  kafka-topics --create --topic "influx-topic" --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
+docker-compose exec kafka  \
+  kafka-topics --describe --topic "influx-topic" --zookeeper zookeeper:32181
+docker-compose exec kafka  \
+  bash -c "echo '{\"clientID\":\"Bob\",\"eventID\":\"Sign In\",\"lat\":\"87687\",\"lon\":\"665.67676\",\"timeSinceEpoch\":\"2018-06-13T01:30:15.01Z\"}' | kafka-console-producer --request-required-acks 1 --broker-list kafka:29092 --topic 'influx-topic'"
+docker-compose exec kafka  \
+  bash -c "echo '{\"clientID\":\"Sally\",\"eventID\":\"Sign In\",\"lat\":\"87687\",\"lon\":\"665.67676\",\"timeSinceEpoch\":\"2018-06-13T01:30:15.01Z\"}' | kafka-console-producer --request-required-acks 1 --broker-list kafka:29092 --topic 'influx-topic'"
+docker-compose exec kafka  \
+  kafka-console-consumer --bootstrap-server kafka:29092 --topic "influx-topic" --from-beginning --max-messages 2
