3f234ab69cff0268f8f61405de87f90432584191
Peter Kaufman <peterjameskaufman@gmail.com>
2018-06-15 12:16:38 -0400

    Peterk/retention policy (#40)
    * Updated docker
    
    Added couchbase to docker and a script file for influxdb's startup
    
    * updated readme
    
    Added details for setting up couchbase to the readme
    
    * Updated docker
    
    Added couchbase to docker and a script file for influxdb's startup
    
    * updated readme
    
    Added details for setting up couchbase to the readme

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 An API to connect clients to a queue to process for live preview.
 
 In /private/etc/hosts, add "kakfa" next to "localhost" so it can choose between hostnames 
+## Setup
+### Couchbase
+After starting Couchbase through docker, go to localhost:8091/ and create an administrator, choose to configure the memory and choose 1000 MB, and then create a user
--- tests/influxDBAddData.sh
+++ tests/influxDBAddData.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-for i in {1..2}
+for i in {1..1000}
 do
     echo $i
-    j=$(($i%10))
+    j=$(($i%100))
     docker-compose exec influx_db \
-        curl -i -XPOST 'http://influx_db:8086/write?db=doppler' -d "influxTest,name=server$j,region=us-west value=$j"
+        curl -i -XPOST 'http://influx_db:8086/write?db=doppler' -d "influxTest,name='Bob',region=$j value=$j"
+done
+for i in {1..1000}
+do
+    echo $i
+    j=$(($i%100))
+    docker-compose exec influx_db \
+        curl -i -XPOST 'http://influx_db:8086/write?db=doppler' -d "influxTest,name='Sally',region=$j value=$j"
 done
