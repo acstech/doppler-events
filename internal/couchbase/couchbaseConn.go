4d10cfb1d7047539c1cdeca3ed2947c0c7349268
pjkaufman <PeterJamesKaufman@gmail.com>
2018-06-18 16:25:31 -0400

    Updated the api
    The API now only accepts clients who have a document that has been made for them. The API also automatically adds the eventID to the couchbase document if it is not already there.

--- internal/couchbase/couchbaseConn.go
+++ internal/couchbase/couchbaseConn.go
@@ -51,7 +51,7 @@ func (c *Couchbase) collectEvents(clientID string) error {
 		return err
 	}
 	// get the Events array into a slice
-	docFrag.Content("Events", &c.Doc.Events)
+	docFrag.Content("Events", &c.Doc.Events) // Error, but why is Doc.Events null?
 	if err != nil {
 		return err
 	}
