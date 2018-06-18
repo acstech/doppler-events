2903473b537168ba8029390f293d9123f9590da8
pjkaufman <PeterJamesKaufman@gmail.com>
2018-06-18 17:51:50 -0400

    added 'config' variable for couchbase

--- cmd/grpcTEST/serviceStart.go
+++ cmd/grpcTEST/serviceStart.go
@@ -4,9 +4,14 @@ import (
 	"github.com/acstech/doppler-events/internal/service"
 	//c meaning client call
 )
-
+const{
+	var COUCHBASE_HOST string = "localhost"
+	var COUCHBASE_BUCKET string = "doppler"
+	var COUCHBASE_USERNAME string = "validator"
+	var COUCHBASE_PASSWORD string = "rotadilav"
+}
 func main() {
-
-	service.Init()
+	
+	service.Init(COUCHBASE_HOST, COUCHBASE_BUCKET, COUCHBASE_USERNAME, COUCHBASE_PASSWORD)
 
 }
