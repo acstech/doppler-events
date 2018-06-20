0c78fbc4ae0a2b074ac86b2f05e4b7931afa1219
Matt Smith <38513513+mattsmith803@users.noreply.github.com>
2018-06-20 09:48:17 -0400

    updated test file so that it provides all possible lat and lng, can also config various number of clients and events per client (#50)

--- cmd/testsend/testSend.go
+++ cmd/testsend/testSend.go
@@ -8,34 +8,45 @@
 package main
 
 import (
-	ptype "github.com/golang/protobuf/ptypes"
+	"fmt"
+	"strconv"
 
 	c "github.com/acstech/doppler-events/cmd/grpcTEST/EventAPIClient" //c meaning client call
+	ptypes "github.com/golang/protobuf/ptypes"
 )
 
 func main() {
 
-	ts := ptype.TimestampNow()
-	//sample data
-	clientID := "test2"
-	eventID := "Sign In"
-	dateTime := ts
+	//testing variables
+	numClients := 1 //the number of clients per test
+	numEvents := 1  //the number of events per client TODO make random number of events
 
-	dataName1 := "lng"
-	dataValue1 := "665.67676"
-	dataName2 := "lat"
-	dataValue2 := "87687"
-	// dataName3 := "gender"
-	// dataValue3 := "male"
+	//data point variables
+	var clientID string
+	var eventID string
+	var dataName1 = "lat"
+	var dataName2 = "lng"
+	var dataSet = make(map[string]string)
 
-	//create map of data
-	dataSet := make(map[string]string)
-	dataSet[dataName1] = dataValue1
-	dataSet[dataName2] = dataValue2
-	// dataSet[dataName3] = dataValue3
-
-	//call to API, requires clientID, eventID, and a dataSet
-	// service.Init()
-	c.DisplayData(clientID, eventID, dateTime, dataSet)
+	//loop to create test data (i is number of data points)
+	//first loop creates number of clients
+	for clientNum := 0; clientNum < numClients; clientNum++ {
+		clientID = fmt.Sprint("client", clientNum)
+		//second loop creates number of events
+		for eventNum := 0; eventNum < numEvents; eventNum++ {
+			eventID = fmt.Sprint("event", eventNum)
+			//third loop iterates through all latitudes and gets current time
+			for lat := -90; lat <= 90; lat++ {
+				dateTime := ptypes.TimestampNow() //get current time
+				dataSet[dataName1] = strconv.Itoa(lat)
+				//fourth loop iterates through all longitudes and calls API
+				for lng := -180; lng <= 180; lng++ {
+					dataSet[dataName2] = strconv.Itoa(lng)
+					//fmt.Println(clientID, " ", eventID, " ", dateTime, " ", dataSet)
+					c.DisplayData(clientID, eventID, dateTime, dataSet)
+				}
+			}
+		}
+	}
 
 }
