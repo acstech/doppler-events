d989680ca1f294b2bc16d702127de364bce6557c
Peter Kaufman <peterjameskaufman@gmail.com>
2018-06-20 12:45:49 -0400

    Updated README (#51)
    * commit to save data
    
    * Updated docker, a test, and the readme
    
    Kafka now makes the topic automatically, the test for sending data works, and the readme now has setup instructions

--- cmd/testsend/testSend.go
+++ cmd/testsend/testSend.go
@@ -10,26 +10,27 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 
-	c "github.com/acstech/doppler-events/cmd/grpcTEST/EventAPIClient" //c meaning client call
-	ptypes "github.com/golang/protobuf/ptypes"
+	c "github.com/acstech/doppler-events/cmd/grpcTEST/EventAPIClient"
+	"github.com/golang/protobuf/ptypes" //c meaning client call
 )
 
 func main() {
-
 	//testing variables
 	numClients := 1 //the number of clients per test
 	numEvents := 1  //the number of events per client TODO make random number of events
 
 	//data point variables
-	var clientID string
-	var eventID string
+	var clientID = "test2"
+	var eventID = "test"
 	var dataName1 = "lat"
 	var dataName2 = "lng"
 	var dataSet = make(map[string]string)
 
-	//loop to create test data (i is number of data points)
-	//first loop creates number of clients
+	// loop to create test data (i is number of data points)
+	// first loop creates number of clients
+	// TODO fix this function
 	for clientNum := 0; clientNum < numClients; clientNum++ {
 		clientID = fmt.Sprint("client", clientNum)
 		//second loop creates number of events
@@ -37,16 +38,16 @@ func main() {
 			eventID = fmt.Sprint("event", eventNum)
 			//third loop iterates through all latitudes and gets current time
 			for lat := -90; lat <= 90; lat++ {
-				dateTime := ptypes.TimestampNow() //get current time
 				dataSet[dataName1] = strconv.Itoa(lat)
 				//fourth loop iterates through all longitudes and calls API
 				for lng := -180; lng <= 180; lng++ {
+					time.Sleep(500 * time.Millisecond)
 					dataSet[dataName2] = strconv.Itoa(lng)
 					//fmt.Println(clientID, " ", eventID, " ", dateTime, " ", dataSet)
+					dateTime := ptypes.TimestampNow() //get current time
 					c.DisplayData(clientID, eventID, dateTime, dataSet)
 				}
 			}
 		}
 	}
-
 }
