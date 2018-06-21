2916ac41045d4e3055303e42f2190cc989668ddd
ben wornom <bworn621@students.bju.edu>
2018-06-21 14:16:21 -0400

    removed log

--- cmd/grpcTEST/serviceStart.go
+++ cmd/grpcTEST/serviceStart.go
@@ -16,7 +16,6 @@ func main() {
 	}
 	//get config variables
 	cbConn := os.Getenv("COUCHBASE_CONN")
-	fmt.Println(cbConn)
 	//pass config variables so that they can be used later
 	err = service.Init(cbConn)
 	// if an error occurred in itialization, shutdown the server
