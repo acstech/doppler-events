563901a4c59d67ec598d8d965cfbb1fb5791b602
ben wornom <bworn621@students.bju.edu>
2018-06-25 08:39:44 -0400

    unit tests

--- cmd/unittests/doppler_test.go
+++ cmd/unittests/doppler_test.go
@@ -1,6 +1,14 @@
 package doppler_test
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/Shopify/sarama"
+	cb "github.com/acstech/doppler-events/internal/couchbase"
+	"github.com/joho/godotenv"
+)
 
 var (
 	bucket      string
@@ -12,10 +20,28 @@ var (
 	dataSet     map[string]string
 	cbconfig    string
 	badcbconfig string
+	producer    sarama.AsyncProducer
 )
 
 func init() {
+	//TODO check if development mode or not?????
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		panic(err)
+	}
+	//get config variables
+	cbConn := os.Getenv("COUCHBASE_CONN")
+	testCB := &cb.Couchbase{Doc: &cb.Doc{}}
+	err = testCB.ConnectToCB(cbConn)
+	if err != nil {
+		//TODO try to manually create CB document, format, recheck error
 
+	}
+
+	config := sarama.NewConfig()
+	brokers := []string{"localhost:9092"}
+	producer, err = sarama.NewAsyncProducer(brokers, config)
 }
 
 //TODO: Test the following
@@ -30,24 +56,32 @@ ConnectToCB
 
 */
 
-func Test1(t *testing.T) {
+func TestInit(t *testing.T) {
+
 	//TODO: Message structure
 	//TODO: couchbase config
 	//TODO: testsend data
 	//TODO: find way to check error to determine if test passes or fails
 
-	//if(functionresult==bad)
+	//if(err := Initialize != nil)
 	//{
 	// t.Errorf("someting wrong")
 	// }
 }
 
-func TestFail1(t *testing.T) {
+func TestInitFail(t *testing.T) {
 	//TODO: some bad data
 	//TODO: some invalid configs
 	//TODO verify error response
+
+	//t.Errorf("something right????")
 }
 
-func Test2(t *testing.T) {
+func TestBatch(t *testing.T) {
+	//  prod :=
+
+}
+
+func TestBatchFail(t *testing.T) {
 
 }
