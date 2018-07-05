92747a927c25eb1fd981df928bf0c286a771044c
deepmicrobe <leanderstevano@gmail.com>
2018-07-05 13:03:22 -0400

    Refactored service.go

---
+++ internal/service/sendToQueue.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
+
+//sendToQueue takes byte array, passes it to producer and writes to kafka instance
+func (prod *Service) sendToQueue(JSONob []byte) {
+	// create message to be sent to kafka
+	msg := &sarama.ProducerMessage{
+		Topic: "influx-topic",
+		Value: sarama.ByteEncoder(JSONob),
+		//Partition: 1, //partNum,
+	}
+	go func() {
+		for err := range prod.producer.Errors() {
+			fmt.Println("Failed to produce message:", err)
+		}
+	}()
+	prod.producer.Input() <- msg
+}
