bf45b2228e8a23da648cf5ef563b4d564df93f56
deepmicrobe <leanderstevano@gmail.com>
2018-07-05 14:23:52 -0400

    Fixed refactoring bugs

--- internal/service/displayData.go
+++ internal/service/displayData.go
@@ -44,10 +44,14 @@ func (s *Service) DisplayData(ctx context.Context, in *pb.DisplayRequest) (*pb.D
 	flatJSONMap := make(map[string]string)
 	//check to make sure that the ClientID exists
 <<<<<<< HEAD
+<<<<<<< HEAD
 	cont, document, err := s.CbConn.ClientExists(in.ClientId)
 =======
 	cont, document, err := s.cbConn.ClientExists(in.ClientId)
 >>>>>>> Refactored service.go
+=======
+	cont, document, err := s.CbConn.ClientExists(in.ClientId)
+>>>>>>> Fixed refactoring bugs
 	if err != nil {
 		if err == gocb.ErrTimeout {
 			return nil, status.Error(codes.Internal, "501: Unable to validate clientID")
@@ -60,11 +64,7 @@ func (s *Service) DisplayData(ctx context.Context, in *pb.DisplayRequest) (*pb.D
 		return nil, status.Error(codes.NotFound, "401: The ClientID is not valid")
 	}
 	//ensure that the eventID exists
-<<<<<<< HEAD
 	err = s.CbConn.EventEnsure(in.ClientId, in.EventId, document)
-=======
-	err = s.cbConn.EventEnsure(in.ClientId, in.EventId, document)
->>>>>>> Refactored service.go
 	if err != nil {
 		//an error ensuring that the event be added to couchbase
 		if err == gocb.ErrTimeout {
